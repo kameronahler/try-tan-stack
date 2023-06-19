2f75b402f3a156a7a3530ee5be755387ab909a51
Kameron Ahler <kamahler@gmail.com>
2023-06-19 15:18:40 -0600

    wip: add dummy form

--- src/PostsList.tsx
+++ src/PostsList.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { getPosts } from './api/posts';
 import { PostsListProps } from './types';
 
@@ -10,24 +11,63 @@ const PostsList = ({ setCurrentPost }: PostsListProps) => {
     queryFn: getPosts,
   });
 
+  const [newTitle, setNewTitle] = useState('');
+  const [newBody, setNewBody] = useState('');
+
   return (
     <>
-      <h1>
-        {postsQuery.isLoading && 'Loading'}
-        {postsQuery.isError && `Error: ${JSON.stringify(postsQuery.error)}`}
-        {postsQuery.isSuccess && 'Success'}
-      </h1>
-      <h2>Posts:</h2>
-      <ol>
-        {postsQuery.isSuccess &&
-          postsQuery.data.map(({ body, id, title }) => (
-            <li key={id}>
-              <button onClick={() => setCurrentPost({ body, title })}>
-                {title}
-              </button>
-            </li>
-          ))}
-      </ol>
+      <header>
+        <h1>
+          {postsQuery.isLoading && 'Loading'}
+          {postsQuery.isError && `Error: ${JSON.stringify(postsQuery.error)}`}
+          {postsQuery.isSuccess && 'Success'}
+        </h1>
+      </header>
+      <section>
+        <h2>Posts:</h2>
+        <ol>
+          {postsQuery.isSuccess &&
+            postsQuery.data.map(({ body, id, title }) => (
+              <li key={id}>
+                <button onClick={() => setCurrentPost({ body, title })}>
+                  {title}
+                </button>
+              </li>
+            ))}
+        </ol>
+      </section>
+      <section>
+        <h2>Add new post:</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <label htmlFor="title">
+              Title:
+              <input
+                id="title"
+                type="text"
+                onChange={(e) => setNewTitle(e.currentTarget.value)}
+                value={newTitle}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="body">
+              Body:
+              <textarea
+                id="body"
+                onChange={(e) => setNewBody(e.currentTarget.value)}
+                value={newBody}
+              />
+            </label>
+          </div>
+          <button
+            onClick={() => window.alert(`${newTitle}, ${newBody}`)}
+            disabled={!newBody || !newTitle}
+          >
+            Submit
+          </button>
+        </form>
+      </section>
     </>
   );
 };
